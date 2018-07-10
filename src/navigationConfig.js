d1dacc7893bb5857659d5b4b3e03e5281795295b
tymiurge <tmrlabas@gmail.com>
2018-07-10 15:16:19 +0300

    navigation config

---
+++ src/navigationConfig.js
@@ -0,0 +1,35 @@
+export default {
+    items: [
+        {
+            name: 'Home',
+            url: '/home',
+            icon: 'icon-speedometer'
+        },
+        {
+            url: '/clients',
+            name: 'Clients',
+            icon: 'icon-notebook'
+        },
+        {
+            url: '/masters',
+            name: 'Masters',
+            icon: 'icon-people'
+        },
+        {
+            url: '/materials',
+            name: 'Materials',
+            icon: 'icon-puzzle'
+        },
+        {
+            url: '/services',
+            name: 'Services',
+            icon: 'icon-diamond'
+        },
+        {
+            url: '/schedules',
+            name: 'Shedules',
+            icon: 'icon-clock'
+        },
+
+    ]
+}
