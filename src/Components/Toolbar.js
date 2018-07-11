f09441f64eca1f757c2cce1b89a6f1545a1943a8
tymiurge <tmrlabas@gmail.com>
2018-07-11 18:02:23 +0300

    Toolbar - toggling states

--- src/Components/Toolbar.js
+++ src/Components/Toolbar.js
@@ -7,30 +7,54 @@ import {
   Button
 } from 'reactstrap'
 
-const Toolbar = () => (
-    <div className={'__toolbar'}>
-    <Button color="primary">
-      <i className="fa fa-search"></i>
-    </Button>
-    <Button color="danger" style={{marginLeft: '5px'}}>
-      <i className="fa fa-plus"></i>
-    </Button>
+class Toolbar extends React.Component {
+
+  state = {
+    buttonsState: true
+  }
+
+  toggleToolbarState = () => this.setState({...this.state, buttonsState: !this.state.buttonsState})
+
+  renderButtonsState = () => (
+    <React.Fragment>
+      <Button color="primary" onClick={ () => this.toggleToolbarState() }>
+        <i className="fa fa-search"></i>
+      </Button>
+      <Button color="danger" style={{marginLeft: '5px'}}>
+        <i className="fa fa-plus"></i>
+      </Button>
+    </React.Fragment>
+  )
+
+  renderSearchState = () => (
+    <InputGroup>
+      <InputGroupAddon addonType="prepend">
+        <InputGroupText>
+          <i className="fa fa-user"></i>
+        </InputGroupText>
+      </InputGroupAddon>
+      <Input type="text" placeholder="Search" />
+      <InputGroupAddon addonType="append">
+        <Button type="button" color="danger" onClick={ () => this.toggleToolbarState() }>
+          <i className="fa fa-times"></i>
+        </Button>
+      </InputGroupAddon>
+    </InputGroup>        
+  )
+
+  render() {
+    return (
+      <div className={'__toolbar'}>
+      {
+        this.state.buttonsState ? this.renderButtonsState() : this.renderSearchState()
+      }  
 
     {/*
-      <InputGroup>
-        <InputGroupAddon addonType="prepend">
-          <InputGroupText>
-            <i className="fa fa-user"></i>
-          </InputGroupText>
-        </InputGroupAddon>
-        <Input type="text" placeholder="Search" />
-      </InputGroup>        
+      
     */}
     </div>
-)
-
-/*
-
-*/
+    )
+  }
+}
 
 export default Toolbar
