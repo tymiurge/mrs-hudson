1da7525a8b4b1b503c560c15f5fd20f119128d62
tymiurge <tmrlabas@gmail.com>
2018-07-14 19:47:20 +0300

    AvatarWizard in Masters

---
+++ src/Pages/Masters/AvatarWizard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card, CardHeader, CardBody, CardImg, Button, CardLink, Input } from 'reactstrap'
+import { Avatar } from './../../Components'
+import noavatar from './../../assets/no-avatar.png'
+import ImageUploader from 'react-images-upload'
+
+class AvatarWizard extends React.Component {
+
+    render() {
+        const btnText = this.props.photo === undefined ? 'Add Photo' : 'Change Photo'
+        return (
+            <Card>
+                <CardHeader>
+                    Avatar
+                </CardHeader>
+                <Avatar src={noavatar} />
+                <CardBody>
+                    {/*
+                        TODO: change the input type='file' look, doc bellow 
+                        https://dev.to/greduan/the-anatomy-of-a-custom-inputtypefile-component
+                    */}
+                    <input type="file" />
+                </CardBody>
+            </Card>
+        )
+    }
+}
+
+export default AvatarWizard
--- src/Pages/Masters/index.js
+++ src/Pages/Masters/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { CardGroup, Row, Col, Card, CardHeader, CardBody } from 'reactstrap'
+import AvatarWizard from './AvatarWizard'
 
-const Masters = props => (<div>masters</div>)
+const Masters = props => (
+    
+    <CardGroup>
+        <AvatarWizard />
+        <Card>
+            <CardHeader>Info</CardHeader>
+        </Card>
+        <Card>
+            <CardHeader>Certificates</CardHeader>
+        </Card>
+        
+    </CardGroup>
+        
+)
 
 export default Masters
