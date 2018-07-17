681419468ddeae7e0e2c5f9a953a90a0c1d2d1c2
tymiurge <tmrlabas@gmail.com>
2018-07-17 13:29:35 +0300

    Certification UI in MasterInfo

---
+++ src/Pages/Masters/PerformanceInfo.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Card, CardHeader, CardBody, CardFooter, Button, Label } from 'reactstrap'
+import Rating from 'react-rating'
+
+import { brands } from 'assets'
+
+const PerformanceInfo = props => (
+    <Card>
+            <CardHeader>Certificates</CardHeader>
+            <CardBody>
+                <Label>Clients rating:</Label>
+                <div>
+                <Rating
+                    emptySymbol='fa fa-star-o fa-2x'
+                    fullSymbol='fa fa-star fa-2x'
+                    initialRating={4}
+                    readonly={true}
+                />
+                </div>
+
+                <Label style={{marginTop: '10px'}}>Certificated by:</Label>
+                <div>
+                    <img src={brands.artnaturals} width={75} />
+                    <img src={brands.loreal} width={75} style={{marginLeft: '10px'}}/>
+                </div>
+
+            </CardBody>
+            <CardFooter>
+                  <Button 
+                    size="sm" 
+                    color="primary"
+                    //onClick={ () => this.props.onEditClick() }
+                  >
+                    <i className="fa fa-info-circle"></i> Details
+                  </Button>
+                </CardFooter>
+        </Card>
+    
+)
+
+export default PerformanceInfo
--- src/Pages/Masters/index.js
+++ src/Pages/Masters/index.js
@@ -2,16 +2,14 @@ import React from 'react'
 import { CardGroup, Card, CardHeader } from 'reactstrap'
 import AvatarWizard from './AvatarWizard'
 import MasterInfo from './MasterInfo'
+import PerformanceInfo from './PerformanceInfo'
 
 const Masters = props => (
 
     <CardGroup>
         <AvatarWizard />
         <MasterInfo />
-        <Card>
-            <CardHeader>Certificates</CardHeader>
-        </Card>
-        
+        <PerformanceInfo />
     </CardGroup>
         
 )
