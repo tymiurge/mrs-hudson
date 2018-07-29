781d984f95a06d8c15898f871027de67904652b1
tymiurge <tmrlabas@gmail.com>
2018-07-29 21:49:46 +0300

    refactoring Components/FlexLayout/Row

---
+++ src/Components/FlexLayout/Row.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * left-to-right oriented flex row. flexWrap is set to 'wrap'
+ */
+class Row extends React.Component {
+
+    render() {
+        return (
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                { this.props.children }
+            </div>
+        )
+    }
+}
+
+export default Row
