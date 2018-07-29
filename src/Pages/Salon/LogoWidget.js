9caad55e8d0ccc5e1dfc3ae5de60e570546654f1
tymiurge <tmrlabas@gmail.com>
2018-07-29 21:57:32 +0300

    LogoWidget in Pages/Salon

--- src/Pages/Salon/LogoWidget.js
+++ src/Pages/Salon/LogoWidget.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PhotoHolder, HorizontalDelimiter, VerticalDelimiter } from 'components'
+import { PhotoHolder, HorizontalDelimiter, VerticalDelimiter, FlexLaoyout } from 'components'
 import {
     Badge,
     Button,
@@ -29,37 +29,53 @@ import {
 
 class LogoWidget extends React.Component {
 
+    state = {
+        staticMode: false
+    }
+
+    toggleMode = () => this.setState({...this.state, staticMode: !this.state.staticMode})
+
     render() {
         return (
             <Card>
                 <CardHeader>
                     Logo
-                    {/*
-                    <div className="card-header-actions">
-                        <a className="card-header-action btn btn-setting" onClick={() => alert('settings clicked')}><i className="icon-settings"></i></a>
-                    </div>
-                    */}
+                    {
+                        !this.state.staticMode &&
+                        <div className="card-header-actions">
+                            <a className="card-header-action btn btn-setting" onClick={ () => this.toggleMode() }><i className="icon-settings"></i></a>
+                        </div>
+                    }
                 </CardHeader>
                 <CardBody>
-                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                        <PhotoHolder title='32x32' width={64} height={64}/>
+                    <FlexLaoyout.Row>
+                        <FlexLaoyout.Column>
+                            <h4>32x32</h4>
+                            <PhotoHolder title='32x32' width={64} height={64}/>
+                        </FlexLaoyout.Column>
                         <VerticalDelimiter />
-                        <PhotoHolder title='150x50'  width={128} height={64}/>
-                    </div>
-                    
+                        <FlexLaoyout.Column>
+                            <h4>150x50</h4>
+                            <PhotoHolder title='150x50'  width={128} height={64}/>
+                        </FlexLaoyout.Column>
+                        
+                    </FlexLaoyout.Row>
                     <HorizontalDelimiter />
-                    <Button color={'success'} size='sm'>
-                        <i className="fa fa-check-square-o"></i>
-                        <span className={'__left_mini_intend'}>32x32</span>
-                    </Button>
-                    <Button className={'__left_intended_item'} color={'danger'} size='sm'>
-                        <i className='fa fa-ban'></i>
-                        <span className={'__left_mini_intend'}>150x50</span>
-                    </Button>
+                    {
+                        this.state.staticMode &&
+                        <React.Fragment>
+                            <Button color={'success'} size='sm'>
+                                <i className="fa fa-check-square-o"></i>
+                                <span className={'__left_mini_intend'}>32x32</span>
+                            </Button>
+                            <Button className={'__left_intended_item'} color={'danger'} size='sm'>
+                                <i className='fa fa-ban'></i>
+                                <span className={'__left_mini_intend'}>150x50</span>
+                            </Button>
+                        </React.Fragment>
+                    }
                 </CardBody>
-
             </Card>
-
         )
     }
 }
